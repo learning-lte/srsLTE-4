5fc44a858ec16d425f1369602306f0a2a03d09c0
ite <zicheng1@umbc.edu>
2019-01-18 13:13:21 -0500

    add traffic measurment tools

---
+++ measurement_tools/top_block.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Thu Jan 17 11:50:29 2019
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import sys
+import time
+from gnuradio import qtgui
+
+
+class top_block(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 1000000
+        self.file2 = file2 = "/home/ite/Documents/traffic measure/test_ch2.txt"
+        self.file1 = file1 = "/home/ite/Documents/traffic measure/test_ch1.txt"
+        self.center_fre_ch2 = center_fre_ch2 = 2477000000
+        self.center_fre = center_fre = 2462000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(2),
+        	),
+        )
+        self.uhd_usrp_source_0.set_subdev_spec('A:A A:B', 0)
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(center_fre, 0)
+        self.uhd_usrp_source_0.set_gain(80, 0)
+        self.uhd_usrp_source_0.set_antenna('RX2', 0)
+        self.uhd_usrp_source_0.set_center_freq(center_fre_ch2, 1)
+        self.uhd_usrp_source_0.set_gain(80, 1)
+        self.uhd_usrp_source_0.set_antenna('RX2', 1)
+        self.blocks_nlog10_ff_0_0 = blocks.nlog10_ff(1, 1, 0)
+        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(1, 1, 0)
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_moving_average_xx_0_0 = blocks.moving_average_ff(40, 1, 4000, 1)
+        self.blocks_moving_average_xx_0 = blocks.moving_average_ff(40, 1, 4000, 1)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_float*1, file2, False)
+        self.blocks_file_sink_0_0.set_unbuffered(False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, file1, False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_conjugate_cc_0_0 = blocks.conjugate_cc()
+        self.blocks_conjugate_cc_0 = blocks.conjugate_cc()
+        self.blocks_complex_to_real_0_0 = blocks.complex_to_real(1)
+        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_moving_average_xx_0, 0))
+        self.connect((self.blocks_complex_to_real_0_0, 0), (self.blocks_moving_average_xx_0_0, 0))
+        self.connect((self.blocks_conjugate_cc_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_conjugate_cc_0_0, 0), (self.blocks_multiply_xx_0_0, 1))
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.blocks_nlog10_ff_0, 0))
+        self.connect((self.blocks_moving_average_xx_0_0, 0), (self.blocks_nlog10_ff_0_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_complex_to_real_0_0, 0))
+        self.connect((self.blocks_nlog10_ff_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_nlog10_ff_0_0, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_conjugate_cc_0, 0))
+        self.connect((self.uhd_usrp_source_0, 1), (self.blocks_conjugate_cc_0_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.uhd_usrp_source_0, 1), (self.blocks_multiply_xx_0_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+
+    def get_file2(self):
+        return self.file2
+
+    def set_file2(self, file2):
+        self.file2 = file2
+        self.blocks_file_sink_0_0.open(self.file2)
+
+    def get_file1(self):
+        return self.file1
+
+    def set_file1(self, file1):
+        self.file1 = file1
+        self.blocks_file_sink_0.open(self.file1)
+
+    def get_center_fre_ch2(self):
+        return self.center_fre_ch2
+
+    def set_center_fre_ch2(self, center_fre_ch2):
+        self.center_fre_ch2 = center_fre_ch2
+        self.uhd_usrp_source_0.set_center_freq(self.center_fre_ch2, 1)
+
+    def get_center_fre(self):
+        return self.center_fre
+
+    def set_center_fre(self, center_fre):
+        self.center_fre = center_fre
+        self.uhd_usrp_source_0.set_center_freq(self.center_fre, 0)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
